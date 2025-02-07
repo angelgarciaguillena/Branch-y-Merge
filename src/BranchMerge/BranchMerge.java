e2dd4d604689ea78c9f2b837f21d69059e5d3a71
angelgarciaguillena <angel.garcia@iesnervion.es>
2025-02-07 12:43:02 +0100

    Cambios

--- src/BranchMerge/BranchMerge.java
+++ src/BranchMerge/BranchMerge.java
@@ -3,5 +3,6 @@ package BranchMerge;
 public class BranchMerge {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Soy Angel y he realizado cambios en la rama");
 	}
 }
